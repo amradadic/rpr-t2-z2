704d061d05a6eb9b33686130c9f2878bff444c9e
adadic1 <44211420+adadic1@users.noreply.github.com>
2019-10-28 11:18:58 +0100

    gotov main zavrsen zadatak

--- src/ba/unsa/etf/rpr/tutorijal02/Main.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Main.java
@@ -1,8 +1,28 @@
 package ba.unsa.etf.rpr.tutorijal02;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Nema ništa u mainu, pokrenite testove!");
+
+        System.out.println("Unesite dva intervala");
+        System.out.print("Pocetna tacka prvog intervala: ");
+        Scanner ulaz = new Scanner(System.in);
+        double pocetna1 = ulaz.nextDouble();
+        System.out.print("Krajnja tacka prvog intervala: ");
+        double krajnja1 = ulaz.nextDouble();
+        Interval interval1 = new Interval(pocetna1, krajnja1, false, false);
+
+        System.out.print("Pocetna tacka drugog intervala: ");
+        double pocetna2 = ulaz.nextDouble();
+        System.out.print("Krajnja tacka drugog intervala: ");
+        double krajnja2 = ulaz.nextDouble();
+        Interval interval2 = new Interval(pocetna2, krajnja2, false, false);
+
+        System.out.print("Presjek intervala: ");
+        System.out.println(interval1.intersect(interval2));
+
     }
 }
